a917e0ab4926fc635b81b097b469600f0afed527
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-10-02 12:36:27 +0200

    martedì
    quasi finito

--- sketch.js
+++ sketch.js
@@ -5,111 +5,91 @@ function preload(){
 function setup() {
   // put setup code here
   createCanvas(windowWidth, windowHeight);
-  background(21,38,60);
+  background(236,236,236);
   angleMode(DEGREES);
   textSize(20);
 
-  frameRate(10);
+  frameRate(15);
+
+
 
 }
 
 function draw() {
 
-  text( frameCount,200,300);
+
+
 
   // put drawing code here
   translate(windowWidth/2,windowHeight/2);
+
+  if (mouseButton == LEFT) {
+    background(236,236,236,275);
+    fill(50);
+      text( "watch the animation, if you're tired left click/right click",-250,windowHeight/4+150);
+      stroke(50);
+  }
+  else if (mouseButton == RIGHT) {
+    background(236,236,236,0);
+    fill(50);
+      text( "watch the animation, if you're tired left click/right click",-250,windowHeight/4+150);
+        stroke(50);
+  }
+  else {
+      background(21,39,61,275-frameCount/2);
+    fill(275);
+        text( "watch the animation, if you're tired left click/right click",-250,windowHeight/4+150);
+       stroke(lerpColor(color('#006783'), color('#ed6d7f'), frameCount/800));
+  }
+
+      //text( frameCount,-250,-windowHeight/4+100);
  // with push() you collect the upcoming transformations
- if (mouseY <= windowHeight/2) {
-   stroke(122,178,205);
-   var angle5 = frameCount*20;
-    var xpos5 =125*cos(angle5);
-    var ypos5 =125*sin(angle5);
-  // draw a line between them
-    stroke(275)
-    line(xpos5, ypos5,125*cos(180), 125*sin(180));
-  
-   else {
-     rect(300, 300, 100, 50); // Lower-right
-   }
-stroke(275);
+
+rotate(frameCount);
  push();
- var angle1 = frameCount*20;
-  var xpos1 =125*cos(angle1);
-  var ypos1 =125*sin(angle1);
+var angle1 = frameCount*15;
+  var xpos1 =200*cos(angle1);
+  var ypos1 =200*sin(angle1);
 // draw a line between them
-  stroke(275)
-  line(xpos1, ypos1,125*cos(0), 125*sin(0));
-if (angle1 == 360) {
-      noLoop();
-    }
-    var angle2 = frameCount*20;
-     var xpos2 =125*cos(angle1);
-     var ypos2 =125*sin(angle1);
+
+  line(xpos1, ypos1,125*cos(frameCount/2), 125*sin(frameCount/2));
+  line(xpos1, ypos1,125*cos(180+frameCount/2), 125*sin(180+frameCount/2));
+rotate(-frameCount);
+
+
+//if (angle1 == 360) {
+      //noLoop();
+    //}
+pop();
+if (keyIsPressed == true) {  // If the key is pressed,
+  background(21,39,61);
+   stroke(275);
+   var angle1 = frameCount*15;
+     var xpos1 =200*cos(angle1);
+     var ypos1 =200*sin(angle1);
    // draw a line between them
-     stroke(275)
-     line(xpos2, ypos2,125*cos(90), 125*sin(90));
-   if (angle1 == 360) {
-         noLoop();
-       }
-   var angle3 = frameCount*20;
-        var xpos3 =125*cos(angle1);
-        var ypos3 =125*sin(angle1);
-      // draw a line between them
-        stroke(275)
-        line(xpos3, ypos3,125*cos(180), 125*sin(180));
-      if (angle1 == 360) {
-            noLoop();
-          }
-          var angle4 = frameCount*20;
-               var xpos4 =125*cos(angle1);
-               var ypos4 =125*sin(angle1);
-             // draw a line between them
-               stroke(275)
-               line(xpos4, ypos4,125*cos(270), 125*sin(270));
-             if (angle1 == 360) {
-                   noLoop();
-                 }
-                 pop();
-                 push();
-                 rotate(45);
-                 var angle1 = frameCount*20;
-                  var xpos1 =125*cos(angle1);
-                  var ypos1 =125*sin(angle1);
-                // draw a line between them
-                  stroke(275)
-                  line(xpos1, ypos1,125*cos(0), 125*sin(0));
-                if (angle1 == 360) {
-                      noLoop();
-                    }
-                    var angle2 = frameCount*20;
-                     var xpos2 =125*cos(angle1);
-                     var ypos2 =125*sin(angle1);
-                   // draw a line between them
-                     stroke(275)
-                     line(xpos2, ypos2,125*cos(90), 125*sin(90));
-                   if (angle1 == 360) {
-                         noLoop();
-                       }
-                   var angle3 = frameCount*20;
-                        var xpos3 =125*cos(angle1);
-                        var ypos3 =125*sin(angle1);
-                      // draw a line between them
-                        stroke(275)
-                        line(xpos3, ypos3,125*cos(180), 125*sin(180));
-                      if (angle1 == 360) {
-                            noLoop();
-                          }
-                          var angle4 = frameCount*20;
-                               var xpos4 =125*cos(angle1);
-                               var ypos4 =125*sin(angle1);
-                             // draw a line between them
-                               stroke(275)
-                               line(xpos4, ypos4,125*cos(270), 125*sin(270));
-                             if (angle1 == 360) {
-                                   noLoop();
-                                 }
-                                 pop();
+
+     line(xpos1, ypos1,125*cos(frameCount/2), 125*sin(frameCount/2));
+     line(xpos1, ypos1,125*cos(180+frameCount/2), 125*sin(180+frameCount/2));
+     line(xpos1, ypos1,125*cos(90+frameCount/2), 125*sin(90+frameCount/2));
+     line(xpos1, ypos1,125*cos(270+frameCount/2), 125*sin(270+frameCount/2));
+     rotate(frameCount/2);
+    noFill();
+     strokeweight(2);
+    beginShape();
+    vertex(125,0);
+    vertex(0,125);
+    vertex(-125,0);
+    vertex(0,-125);
+    endShape();
+    rotate(-frameCount/2);
+  }
+  else {
+    rotate(frameCount*2);
+    noFill();                    // Otherwise,
+  ellipse(250,0,25,25);
+    rotate(-frameCount*2);     // draw a rectangle.
+  }
 
 
 
